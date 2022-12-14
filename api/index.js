6e94afdf8b9d1d64c5e31f74cbd7fe3ece985cd7
Vitalii <rebrun508@gmail.com>
2022-12-14 14:05:46 +0200

    update api

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-import express from "express";clearImmediate
+import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import authRoute from "./routes/auth.js";
@@ -12,41 +12,40 @@ const app = express();
 dotenv.config();
 
 const connect = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO);
-        console.log("Connected to MongoDB")
-    } catch (error) {
-        throw error;
-    }
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to mongoDB.");
+  } catch (error) {
+    throw error;
+  }
 };
 
 mongoose.connection.on("disconnected", () => {
-    console.log("mongoDB disconnected")
-})
+  console.log("mongoDB disconnected!");
+});
 
 //middlewares
 app.use(cors())
 app.use(cookieParser())
-app.use(express.json())
+app.use(express.json());
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/hotels", hotelsRoute);
 app.use("/api/rooms", roomsRoute);
 
-app.use((err,req,res,next) => {
-    const errorStatus = err.status || 500
-    const errorMessage = err.message || 'Something went wrong'
-    return res.status(errorStatus).json({
-        success:false,
-        status: errorStatus,
-        message: errorMessage,
-        stack: err.stack,
-
-    })
-})
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
 
 app.listen(8800, () => {
-    connect()
-    console.log("Connected to backend.")
-}) 
+  connect();
+  console.log("Connected to backend.");
+});
