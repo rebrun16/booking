f21a7393a9ca594db5d830b4aaa80c1a679e44f5
Vitalii <rebrun508@gmail.com>
2022-12-19 21:12:41 +0200

    +

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,23 @@
 import {
-  // BrowserRouter,
+  BrowserRouter,
   Routes,
   Route,
-  HashRouter,
 } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Hotel from "./pages/hotel/Hotel";
 import List from "./pages/list/List";
 import Login from "./pages/login/Login";
 
-const  App = () => {
+function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/hotels" element={<List/>}/>
         <Route path="/hotels/:id" element={<Hotel/>}/>
         <Route path="/login" element={<Login/>}/>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
