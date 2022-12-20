ffa0fea5c7de26f81f6b78a801fb068286a83476
Vitalii <rebrun508@gmail.com>
2022-12-20 23:09:49 +0200

    update

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { AuthContextProvider } from './context/AuthContext';
 import { SearchContextProvider } from './context/SearchContext';
@@ -9,13 +9,13 @@ import { SearchContextProvider } from './context/SearchContext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <BrowserRouter> */}
+    <BrowserRouter>
       <AuthContextProvider>
         <SearchContextProvider>
           <App />
         </SearchContextProvider>
       </AuthContextProvider>
-    {/* </BrowserRouter> */}
+    </BrowserRouter>
   </React.StrictMode>
 );
 
